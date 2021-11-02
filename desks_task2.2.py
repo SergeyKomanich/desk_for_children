9a87faa891354d44ff32de2141a96f2ebc1aa573
SergeyKomanich <komanichs@gmail.com>
2021-11-02 09:43:17 +0200

    homework

--- desks_task2.2.py
+++ desks_task2.2.py
@@ -13,10 +13,10 @@
 math_class1 = int(input("Enter the number of students: "))
 math_class2 = int(input("Enter the number of students: "))
 math_class3 = int(input("Enter the number of students: "))
-
-#Узнаем общее количество учеников
-total_number_of_pupils = math_class1 + math_class2 + math_class3
-
-# вычисляем количество парт (2-школьника на парту), сколько их останется после деления на 2, складываем эти два значения
-number_of_desks = total_number_of_pupils // 2 + total_number_of_pupils % 2
+# вычисляем количество парт (2-школьника на парту)
+desks_for_class1 = math_class1 // 2 + math_class1 % 2
+desks_for_class2 = math_class2 // 2 + math_class2 % 2
+desks_for_class3 = math_class3 // 2 + math_class3 % 2
+#Узнаем общее количество парт
+number_of_desks = desks_for_class1 + desks_for_class2 + desks_for_class3
 print("Desks need: ", number_of_desks)
